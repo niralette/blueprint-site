77cfcafdd30f74b1b02f7720933bf06965fa7654
niralette <167697405+niralette@users.noreply.github.com>
2024-05-28 21:14:26 +0100

    refactor: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import clipboard from '../assets/clipboard.webp';
+import coupling from '../assets/minecart_coupling.webp';
+import logo from '../assets/logo.webp';
+import schematic from '../assets/schematic.webp';
+
+interface Link {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export function Header() {
+  const links: Link[] = [
+    {
+      icon: coupling,
+      title: 'Addons',
+      url: '/addons',
+    },
+    {
+      icon: schematic,
+      title: 'Schematics',
+      url: '/schematics',
+    },
+    {
+      icon: clipboard,
+      title: 'About',
+      url: '/about',
+    },
+  ];
+
+  return (
+    <header>
+      <div className="container mx-auto flex h-16 w-full items-center justify-between">
+        <a className="font-minecraft flex items-center gap-3 text-xl" href="/">
+          <img className="inline-block h-10" src={logo} alt="Blueprint Logo" />
+          <span>Blueprint</span>
+        </a>
+
+        <nav>
+          <ul className="flex items-center gap-5">
+            {links.map(link => (
+              <li key={link.url}>
+                <a className="flex gap-2" href={link.url}>
+                  <img
+                    className="inline-block h-6"
+                    src={link.icon}
+                    alt=""
+                    aria-hidden="true"
+                  />
+
+                  <span>{link.title}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
