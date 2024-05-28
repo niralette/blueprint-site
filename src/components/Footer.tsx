fd3903e9926a9c2fd08f4c4cdd06ce03856ddb91
niralette <167697405+niralette@users.noreply.github.com>
2024-05-28 21:23:02 +0100

    refactor: add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,27 @@
+import logo from '../assets/logo.webp';
+
 export function Footer() {
-  return null;
+  return (
+    <footer className="mt-10 bg-black py-4 text-white">
+      <div className="container mx-auto">
+        <h2 className="sr-only">Footer</h2>
+
+        <div>
+          <img className="h-10" src={logo} alt="Blueprint Logo" />
+        </div>
+
+        <p className="font-bold">Create Addons</p>
+
+        <p className="text-xs">
+          Found a bug? Report it to{' '}
+          <a href="mailto:blueprint-site@proton.me">blueprint-site@proton.me</a>
+        </p>
+
+        <p className="text-xs">
+          Not an official Minecraft service. Not approved by or associated with
+          Mojang or Microsoft
+        </p>
+      </div>
+    </footer>
+  );
 }
